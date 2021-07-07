79b780db54ac8e96c02a2d528f3121aeccd38a53
Pralhad <Pralhadkharel@gmail.com>
2021-07-07 11:34:45 +0545

    mockClear

--- src/test/Function/1.test.ts
+++ src/test/Function/1.test.ts
@@ -1,12 +1,7 @@
-
-
 import { jest, describe, test, expect ,beforeEach} from '@jest/globals'
-
 function a(cb:any,x:any) {
   cb(x)
 }
-
-
 const mockFn = jest.fn();
 
 beforeEach(() => {
@@ -24,16 +19,29 @@ describe("Testing clearMocks", () => {
   });
   test('Testing twice', () => {
     // mockFn.mockClear();
-  a(mockFn,4) 
-  expect(mockFn).toHaveBeenCalledWith(4);
+    a(mockFn, 4)
+    console.log('mockFn', mockFn.mock)
+    // {
+    //   calls: [ [ 4 ] ],
+    //   instances: [ undefined ],
+    //   invocationCallOrder: [ 2 ],
+    //   results: [ { type: 'return', value: undefined } ]
+    expect(mockFn).toHaveBeenCalledWith(4);
     expect(mockFn).toHaveBeenCalledTimes(1); //error occurs when mockClear is not used
     // Expected number of calls: 1
     // Received number of calls: 2
-});
+  });
   test('Testing thrice', () => {
     // mockFn.mockClear();
-  a(mockFn,4)
-  expect(mockFn).toHaveBeenCalledWith(4);
+    a(mockFn,3)   
+    console.log('mockFn', mockFn.mock)
+  //   {
+  //   calls: [ [ 3 ] ],
+  //   instances: [ undefined ],
+  //   invocationCallOrder: [ 3 ],
+  //   results: [ { type: 'return', value: undefined } ]
+  // }
+  expect(mockFn).toHaveBeenCalledWith(3);
     expect(mockFn).toHaveBeenCalledTimes(1); //error occurs when mockClear is not used
     // Expected number of calls: 1
     // Received number of calls: 2
